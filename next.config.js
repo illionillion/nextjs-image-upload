8d2972efe0d4d3b83f73821ef98b49fcfdb86ed5
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-19 08:00:56 +0900

    formidableが使えない

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `formidable` module
+    if (!isServer) {
+      config.node = {
+        formidable: 'empty'
+      }
+    }
+
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import formidable from "formidable";
 type Data = {
   name: string;
 };
@@ -7,7 +8,26 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
 
-  console.log(req.body);
-  
-  res.status(200).json({ name: "!!!" });
+  // console.log(req.body);
+  if (req.method !== "POST") return;
+
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, async function (err, fields, files) {
+    if (err) {
+      res.status(500).end();
+      // res.json({
+      //   method: req.method,
+      //   error: err
+      // });
+      // res.end();
+      return;
+    }
+    // const file = files.file;
+    console.log(req.body);
+    console.log(files);
+    
+
+    res.status(200).json({ name: "!!!" });
+  })
 }
