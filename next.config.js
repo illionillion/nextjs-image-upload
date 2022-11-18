cff12347c051ca84c0dcadc2f5999fc38603d9f3
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-19 08:36:30 +0900

    エラー出るのでコメントアウト

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `formidable` module
-    if (!isServer) {
-      config.node = {
-        formidable: 'empty'
-      }
-    }
+  // webpack: (config, { isServer }) => {
+  //   // Fixes npm packages that depend on `formidable` module
+  //   if (!isServer) {
+  //     config.node = {
+  //       formidable: 'empty'
+  //     }
+  //   }
 
-    return config
-  }
+  //   return config
+  // }
 }
 
 module.exports = nextConfig
