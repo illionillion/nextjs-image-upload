0743c118d3f20291d6562e813b886446a989cf34
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-19 16:30:21 +0900

    いらないので一部削除

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // webpack: (config, { isServer }) => {
-  //   // Fixes npm packages that depend on `formidable` module
-  //   if (!isServer) {
-  //     config.node = {
-  //       formidable: 'empty'
-  //     }
-  //   }
-
-  //   return config
-  // }
 }
 
 module.exports = nextConfig
